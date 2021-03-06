435a5241f86a0e332aad24cd0f1684636cd0a353
Petricevic Luka <luka.petricevic00@fesb.hr>
2021-03-06 21:32:20 +0100

    Added TipsAdminController

---
+++ BookmakersApplication/Controllers/TipsAdminController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BookmakersApplication.BookmakerContext;
+using BookmakersApplication.Models;
+
+namespace BookmakersApplication.Controllers
+{
+    public class TipsAdminController : Controller
+    {
+        private BookmakerDbContext db = new BookmakerDbContext();
+
+        // GET: TipsAdmin
+        public ActionResult Index()
+        {
+            return View(db.Tips.ToList());
+        }
+
+        // GET: TipsAdmin/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tip tip = db.Tips.Find(id);
+            if (tip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tip);
+        }
+
+        // GET: TipsAdmin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TipsAdmin/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TipId,Pair,Quota1,Quota1X,QuotaX,QuotaX2,Quota2,IsTopOffer,Sport,Status,Result")] Tip tip)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tips.Add(tip);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tip);
+        }
+
+        // GET: TipsAdmin/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tip tip = db.Tips.Find(id);
+            if (tip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tip);
+        }
+
+        // POST: TipsAdmin/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TipId,Pair,Quota1,Quota1X,QuotaX,QuotaX2,Quota2,IsTopOffer,Sport,Status,Result")] Tip tip)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tip).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tip);
+        }
+
+        // GET: TipsAdmin/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tip tip = db.Tips.Find(id);
+            if (tip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tip);
+        }
+
+        // POST: TipsAdmin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tip tip = db.Tips.Find(id);
+            db.Tips.Remove(tip);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
